76bcba9a3ef18d0d69024749cd40feb6e1df1c00
sangram0211 <sangramshelke070@gmail.com>
2026-04-23 17:50:33 +0530

    ch

--- server.js
+++ server.js
@@ -55,6 +55,6 @@ require('./sockets/socketHandler')(io);
 
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT} and bound to 0.0.0.0`);
 });
